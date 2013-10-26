ad96ee287c7033f65759946978caf8c1654374ed
guillermo grillo <guillermo.grillo88@gmail.com>
2013-10-25 23:35:22 -0300

    Modifico los tests de cache, para que redefinan la clase y borren al finalizar, para evitar efecto colateral sobre el fixture

--- test/cache_spec.rb
+++ test/cache_spec.rb
@@ -1,20 +1,46 @@
 require 'rspec'
 
 require_relative '../src/cache'
-require_relative 'fixture'
 
 describe StatelessCache do
 
+  before do
+    class Perro
+
+      attr_accessor :nombre, :edad
+
+      def self.de_clase
+        puts 'Este es un metodo de clase definido en Perro'
+      end
+
+      def hacer_algo
+        puts 'Haciendo algo'
+      end
+
+      def hacer_algo_de_una_forma(forma)
+        puts "Haciendo algo de la forma: #{forma}"
+      end
+
+      def devolver_random
+        rand(1000000)
+      end
+    end
+  end
+
+  after do
+    Object.send :remove_const, :Perro
+  end
+
   it 'debe cachear la invocacion de metodos' do
-    StatelessCache.advice.modificar(Persona, Persona.instance_method(:devolver_random))
-    persona = Persona.new
+    StatelessCache.advice.modificar(Perro, Perro.instance_method(:devolver_random))
+    perro = Perro.new
     StatelessCache.cache.length.should == 0
-    cacheado = persona.devolver_random
+    cacheado = perro.devolver_random
     StatelessCache.cache.length.should == 1
-    cacheado.should == persona.devolver_random
+    cacheado.should == perro.devolver_random
     StatelessCache.cache.length.should == 1
-    otra_persona = Persona.new
-    cacheado.should == persona.devolver_random
+    otroPerro = Perro.new
+    cacheado.should == otroPerro.devolver_random
     StatelessCache.cache.length.should == 1
   end
 
@@ -22,24 +48,51 @@ end
 
 describe StatefulCache do
 
+  before do
+    class Perro
+
+      attr_accessor :nombre, :edad
+
+      def self.de_clase
+        puts 'Este es un metodo de clase definido en Perro'
+      end
+
+      def hacer_algo
+        puts 'Haciendo algo'
+      end
+
+      def hacer_algo_de_una_forma(forma)
+        puts "Haciendo algo de la forma: #{forma}"
+      end
+
+      def devolver_random
+        rand(1000000)
+      end
+    end
+  end
+
+  after do
+    Object.send :remove_const, :Perro
+  end
+
   it 'debe cachear la invocacion con estado' do
-    StatefulCache.advice.modificar(Persona,Persona.instance_method(:hacer_algo))
-    persona = Persona.new
-    persona.nombre = 'Juan'
-    persona.apelido = 'Perez'
-    otra_persona = Persona.new
-    otra_persona.nombre = 'Juan'
-    otra_persona.apelido = 'Perez'
+    StatefulCache.advice.modificar(Perro,Perro.instance_method(:hacer_algo))
+    perro = Perro.new
+    perro.nombre = 'Juan'
+    perro.edad = 7
+    otro_perro = Perro.new
+    otro_perro.nombre = 'Juan'
+    otro_perro.edad = 7
     StatefulCache.cache.length.should == 0
-    cacheado = persona.hacer_algo
+    cacheado = perro.hacer_algo
     StatefulCache.cache.length.should == 1
-    cacheado.should == persona.hacer_algo
+    cacheado.should == perro.hacer_algo
     StatefulCache.cache.length.should == 1
-    cacheado.should == otra_persona.hacer_algo
+    cacheado.should == otro_perro.hacer_algo
     StatefulCache.cache.length.should == 1
-    otra_persona.nombre = 'Cacho'
-    otra_persona.apelido = 'Perez'
-    cacheado.should == otra_persona.hacer_algo
+    otro_perro.nombre = 'Cacho'
+    otro_perro.edad = 8
+    cacheado.should == otro_perro.hacer_algo
     StatefulCache.cache.length.should == 2
   end
 
